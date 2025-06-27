bc8a79630780317d80401fa37d80079d346285e8
AlysonRyan01 <AlysonRyan01@users.noreply.github.com>
2025-06-27 23:13:06 +0000

    deploy: f67924186e1a7544dc740f628452777fa9def256

---
+++ js/scroll.js
@@ -0,0 +1,12 @@
+﻿window.scrollToId = (id) => {
+    const el = document.getElementById(id);
+    if (el) {
+        const elementPosition = el.getBoundingClientRect().top + window.pageYOffset;
+        const offsetPosition = elementPosition
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth"
+        });
+    }
+};
